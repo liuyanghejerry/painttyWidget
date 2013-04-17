c87dda679f6c5dcab7e23c9368feade7e42450f9
liuyanghejerry <liuyanghejerry@126.com>
2013-04-17 19:51:11 +0800

    minor change.
    Signed-off-by: liuyanghejerry <liuyanghejerry@126.com>

--- common.h
+++ common.h
@@ -12,7 +12,7 @@ const static char CLIENT_TYPE[] = "alpha";
 const static char SETTINGS_NAME[] = "mrpaint.ini";
 
 #ifdef PAINTTY_DEV
-const static char HOST_ADDR[] = "198.211.105.150";
+const static char HOST_ADDR[] = "42.121.85.47";
 const static int HOST_MGR_PORT = 7070;
 #else
 const static char HOST_ADDR[] = "42.121.85.47";
