bfbe2bbfc61e61fcb65d16eba60486f29a167f26
liuyanghejerry <liuyanghejerry@126.com>
2013-05-10 17:54:30 +0800

    update: translations.
    Signed-off-by: liuyanghejerry <liuyanghejerry@126.com>

--- src/painttyUpdater/translation/updater_ja.ts
+++ src/painttyUpdater/translation/updater_ja.ts
@@ -4,21 +4,21 @@
 <context>
     <name>Updater</name>
     <message>
-        <location filename="../updater.cpp" line="112"/>
+        <location filename="../updater.cpp" line="113"/>
         <source>New version!</source>
-        <translation type="unfinished"></translation>
+        <translation>新バージョン！</translation>
     </message>
     <message>
-        <location filename="../updater.cpp" line="115"/>
+        <location filename="../updater.cpp" line="116"/>
         <source>There&apos;s a new version of Mr.Paint.
 We suggest you download it &lt;a href=&apos;%1&apos;&gt;here&lt;/a&gt;.</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../updater.cpp" line="120"/>
+        <location filename="../updater.cpp" line="121"/>
         <source>There&apos;s a critical update of Mr.Paint.
 You can connect to server ONLY if you&apos;ve &lt;a href=&apos;%1&apos;&gt;updated&lt;/a&gt;!</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 </TS>
--- src/painttyUpdater/translation/updater_zh_CN.ts
+++ src/painttyUpdater/translation/updater_zh_CN.ts
@@ -4,18 +4,18 @@
 <context>
     <name>Updater</name>
     <message>
-        <location filename="../updater.cpp" line="112"/>
+        <location filename="../updater.cpp" line="113"/>
         <source>New version!</source>
         <translation>新版本！</translation>
     </message>
     <message>
-        <location filename="../updater.cpp" line="115"/>
+        <location filename="../updater.cpp" line="116"/>
         <source>There&apos;s a new version of Mr.Paint.
 We suggest you download it &lt;a href=&apos;%1&apos;&gt;here&lt;/a&gt;.</source>
         <translation>发现茶绘君的新版本，我们建议您进行&lt;a href=&apos;%1&apos;&gt;更新&lt;/a&gt;。</translation>
     </message>
     <message>
-        <location filename="../updater.cpp" line="120"/>
+        <location filename="../updater.cpp" line="121"/>
         <source>There&apos;s a critical update of Mr.Paint.
 You can connect to server ONLY if you&apos;ve &lt;a href=&apos;%1&apos;&gt;updated&lt;/a&gt;!</source>
         <translation>发现茶绘君的新版本，只有使用&lt;a href=&apos;%1&apos;&gt;新版本&lt;/a&gt;才能连接到服务器！</translation>
