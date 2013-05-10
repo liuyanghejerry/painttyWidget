1b03001d67e888e62a106b66a2a25540fada65ee
liuyanghejerry <liuyanghejerry@126.com>
2013-05-10 22:02:03 +0800

    change: macro; change: use aliyun server for a while.
    Signed-off-by: liuyanghejerry <liuyanghejerry@126.com>

--- src/common/common.h
+++ src/common/common.h
@@ -14,8 +14,10 @@ static const char UPDATER_VERSION[] = "0.1";
 const static char SETTINGS_NAME[] = "mrpaint.ini";
 
 #ifdef PAINTTY_DEV
-const static QString HOST_ADDR_IPV4("106.187.53.78");
-const static QString HOST_ADDR_IPV6("2400:8900::f03c:91ff:fe70:bc64");
+const static QString HOST_ADDR_IPV4("42.121.85.47");
+const static QString HOST_ADDR_IPV6("42.121.85.47");
+//const static QString HOST_ADDR_IPV4("106.187.53.78");
+//const static QString HOST_ADDR_IPV6("2400:8900::f03c:91ff:fe70:bc64");
 const static int HOST_MGR_PORT = 7070;
 const static int HOST_UPDATER_PORT = 7071;
 #else
@@ -28,11 +30,11 @@ const static int HOST_UPDATER_PORT = 7071;
 const static QString HOST_ADDR[] = {HOST_ADDR_IPV4,
                                     HOST_ADDR_IPV6};
 									
-#ifdef Q_OS_WIN32
+#if defined(Q_OS_WIN32)
 static const char* DOWNLOAD_URL = "http://mrspaint.oss.aliyuncs.com/%E8%8C%B6%E7%BB%98%E5%90%9B_Alpha_x86.zip";
-#elif Q_OS_UNIX
+#elif defined(Q_OS_UNIX) && !defined(Q_OS_MACX)
 static const char* DOWNLOAD_URL = "";
-#elif Q_OS_MAC
+#elif defined(Q_OS_MACX)
 static const char* DOWNLOAD_URL = "http://mrspaint.oss.aliyuncs.com/%E8%8C%B6%E7%BB%98%E5%90%9B.app.zip";
 #endif
 
