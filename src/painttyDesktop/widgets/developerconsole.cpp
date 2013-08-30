28c280ebe33283daac60dc4d33dd1939c12152dc
liuyanghejerry <liuyanghejerry@126.com>
2013-08-30 21:32:55 +0800

    minor changes on developer console.
    Signed-off-by: liuyanghejerry <liuyanghejerry@126.com>

--- src/painttyDesktop/widgets/developerconsole.cpp
+++ src/painttyDesktop/widgets/developerconsole.cpp
@@ -1,6 +1,7 @@
 #include "developerconsole.h"
 #include "ui_developerconsole.h"
 #include <QDebug>
+#include <QScrollBar>
 
 DeveloperConsole::DeveloperConsole(QWidget *parent) :
     QDialog(parent),
@@ -24,14 +25,33 @@ void DeveloperConsole::commandProc()
     if(commandLine.isEmpty()){
         return;
     }
+
+    QTextCursor c = ui->console->textCursor();
+    c.movePosition(QTextCursor::End);
+    ui->console->setTextCursor(c);
+
     ui->console->insertPlainText(QString("%1%2%3")
                                  .arg(">")
                                  .arg(commandLine)
                                  .arg("\n"));
+    ui->console->verticalScrollBar()
+            ->setValue(ui->console->verticalScrollBar()
+                       ->maximum());
+
     emit evaluate(commandLine);
 }
 
 void DeveloperConsole::append(const QString &content)
 {
+    QTextCursor c = ui->console->textCursor();
+    c.movePosition(QTextCursor::End);
+    ui->console->setTextCursor(c);
+
     ui->console->insertPlainText(content+"\n");
+
+    ui->console->verticalScrollBar()
+            ->setValue(ui->console->verticalScrollBar()
+                       ->maximum());
+
+
 }
