d81bf1c061839d45ef4d7dc231a1715f301e0a17
liuyanghejerry <liuyanghejerry@126.com>
2013-10-20 16:06:20 +0800

    update: errcode message.
    Signed-off-by: liuyanghejerry <liuyanghejerry@126.com>

--- src/painttyDesktop/misc/errortable.cpp
+++ src/painttyDesktop/misc/errortable.cpp
@@ -73,6 +73,8 @@ QString ErrorTable::toString(int errorCode)
         return QObject::tr("room is full.");
     case 304:
         return QObject::tr("you're banned.");
+    case 305:
+        return QObject::tr("server is too busy.");
     case 500:
         return QObject::tr("closed by room or room manager.");
     case 501:
@@ -81,6 +83,18 @@ QString ErrorTable::toString(int errorCode)
         return QObject::tr("unknown error.");
     case 601:
         return QObject::tr("room is closed already.");
+    case 700:
+        return QObject::tr("unknown error.");
+    case 701:
+        return QObject::tr("wrong key.");
+    case 702:
+        return QObject::tr("timeout, which means too late.");
+    case 800:
+        return QObject::tr("unknown error.");
+    case 900:
+        return QObject::tr("unknown error.");
+    case 901:
+        return QObject::tr("out of range.");
     default:
         return QObject::tr("Unnamed error: %1.").arg(errorCode);
     }
