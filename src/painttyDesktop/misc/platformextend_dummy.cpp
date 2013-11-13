e5aac141f15fcad49a0dd404a1a8be1e2797b442
Yanghe Liu <liuyanghejerry@126.com>
2013-11-13 09:36:17 +0800

    update: use alert as default notification.

--- src/painttyDesktop/misc/platformextend_dummy.cpp
+++ src/painttyDesktop/misc/platformextend_dummy.cpp
@@ -1,8 +1,9 @@
 #include "platformextend.h"
+#include <QApplication>
 
-void PlatformExtend::notify(QWidget *, int )
+void PlatformExtend::notify(QWidget *widget, int times)
 {
-    //
+     QApplication::alert(widget, times);
 }
 
 bool PlatformExtend::setIMEState(QWidget *, bool st)
