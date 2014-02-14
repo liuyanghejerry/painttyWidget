524becfe27c5e6bc6be4eccd87dd2542f01b41ec
liuyanghejerry <liuyanghejerry@126.com>
2014-02-14 14:35:15 +0800

    更新版本号
    从0.5到0.51

--- src/common/common.h
+++ src/common/common.h
@@ -9,7 +9,7 @@ namespace GlobalDef
 {
 
 const static char CLIENT_TYPE[] = "alpha";
-const static int CLIENT_VER = 50;
+const static int CLIENT_VER = 51;
 
 const static char SETTINGS_NAME[] = "mrpaint.ini";
 
