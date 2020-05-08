e047c34c13be0959811ec9504407d534203d39e7
hathlife <cjjmpgjjl.113@gmail.com>
2020-05-09 01:04:17 +0800

    Generalized deb control file.

--- src/painttyDesktop/package_deb.sh
+++ src/painttyDesktop/package_deb.sh
@@ -4,7 +4,11 @@
 
 VER=$(cat deb_package/DEBIAN/control | grep "Version" | awk '{print $2}')
 
-ARCH=$(cat deb_package/DEBIAN/control | grep "Architecture" | awk '{print $2}')
+ARCH=$(dpkg -l apt | sed -n '6p' | awk '{print $4}')
+
+QTVER=$(qmake -v | sed -n '2p' | awk '{print $4}')
+
+COMMIT=$(git log | head -1 | awk '{print $2}' | cut -c1-7)
 
 rm -rf /tmp/deb_package
 
@@ -12,9 +16,21 @@ cp -r deb_package /tmp
 
 cp ./iconset/painttyWidget.ico /tmp/deb_package/usr/share/pixmap/painttyWidget.ico
 
+INSTALLSIZE=$(du -sk /tmp/deb_package/usr | awk '{print $1}')
+
+sed -i "s/ARCH/${ARCH}/g" /tmp/deb_package/DEBIAN/control 
+
+sed -i "s/QTVER/${QTVER}/g" /tmp/deb_package/DEBIAN/control 
+
+sed -i "/Version/ s/$/-${COMMIT}/" /tmp/deb_package/DEBIAN/control
+  
+sed -i "4 a Installed-Size: ${INSTALLSIZE}" /tmp/deb_package/DEBIAN/control
+
+cat /tmp/deb_package/DEBIAN/control
+
 chmod -R 0755 /tmp/deb_package
 
-dpkg-deb --build /tmp/deb_package ../../build/painttyDesktop/mrpaint_${VER}_${ARCH}.deb
+dpkg-deb --build /tmp/deb_package ../../build/painttyDesktop/mrpaint_${VER}_${ARCH}_${COMMIT}.deb
 
 rm -rf /tmp/deb_package
 
